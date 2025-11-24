b5a4a600f607963544d73e092b19880f9d514e07
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-24 14:37:52 -0700

    Move Passport service to its own directory

--- index.js
+++ index.js
@@ -6,22 +6,6 @@ const keys = require("./config/keys");
 const PORT = process.env.PORT || 5001;
 const app = express();
 
-// Authenticate users with Google Strategy. Function in second argument is where app can get user details, create new record in database, etc.
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("access token:", accessToken);
-      console.log("refresh token:", refreshToken);
-      console.log("profile:", profile);
-    }
-  )
-);
-
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
 });
