74f3a239c90c1f8963f117a2ad47151ce5d4f300
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-24 14:51:17 -0700

    Refactor index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 require("./services/passport");
-const authRoutes = require("./routes/authRoutes");
 
 const PORT = process.env.PORT || 5001;
 const app = express();
 
-authRoutes(app);
+require("./routes/authRoutes")(app); // Require in this module and call its function with app as its argument
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
