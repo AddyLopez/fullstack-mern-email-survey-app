be517c851f7b80b59d6aae916f09d7446c84fa21
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-24 11:43:44 -0700

    Update Port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
-app.listen(port, () => {
-  console.log(`Listening on Port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
 });
