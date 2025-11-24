5e1883a4bca16cef3d471c02e5b1018cec46f2e2
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-24 13:41:50 -0700

    Add new Google Strategy to authenticate users

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-const app = express();
 const PORT = process.env.PORT || 5001;
+const app = express();
+
+passport.use(new GoogleStrategy()); // Authenticate users with Google
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
