383d110d893f2cef0057a40e47d8116696ffcf99
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-24 14:49:49 -0700

    Export routes as a module

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require("./services/passport");
+const authRoutes = require("./routes/authRoutes");
 
 const PORT = process.env.PORT || 5001;
 const app = express();
+
+authRoutes(app);
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,14 @@
-// Whenever a user visits this endpoint, attempt to authenticate that user (no code property has been sent from Google yet)
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
+const passport = require("passport"); // Require in original Passport NPM module here (not directly related to Passport config in services/passport.js)
 
-// In this case, Passport will see the code property sent from Google. Exchange code for user's profile.
-app.get("/auth/google/callback", passport.authenticate("google"));
+module.exports = (app) => {
+  // Whenever a user visits this endpoint, attempt to authenticate that user (no code property has been sent from Google yet)
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
+
+  // In this case, Passport will see the code property sent from Google. Exchange code for user's profile.
+  app.get("/auth/google/callback", passport.authenticate("google"));
+};
