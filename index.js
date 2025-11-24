cd2a1d04addd066f105d4c15aa9ee426486675b9
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-24 14:13:03 -0700

    Add new GET route to OAuth flow

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const keys = require("./config/keys");
 const PORT = process.env.PORT || 5001;
 const app = express();
 
-// Authenticate users with Google
+// Authenticate users with Google Strategy
 passport.use(
   new GoogleStrategy(
     {
@@ -20,13 +20,16 @@ passport.use(
   )
 );
 
-// Whenever a user visits this endpoint, attempt to authenticate that user
+// Whenever a user visits this endpoint, attempt to authenticate that user (no code property has been sent from Google yet)
 app.get(
   "/auth/google",
   passport.authenticate("google", {
     scope: ["profile", "email"],
   })
 );
+
+// In this case, Passport will see the code property sent from Google. Exchange code for user's profile.
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
