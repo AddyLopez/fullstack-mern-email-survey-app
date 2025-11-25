62180b08e432b08fd56b21ff121cb771bbe310eb
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-24 19:03:46 -0700

    Configure cookie with expiration and encryption

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -8,6 +10,14 @@ mongoose.connect(keys.mongoURI); // Use Mongoose to connect with MongoDB instanc
 
 const PORT = process.env.PORT || 5001;
 const app = express();
+
+// Cookie automatically expires after 30 days. Keys property encrypts the cookie.
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
 
 require("./routes/authRoutes")(app); // Require in this module and call its function with app as its argument
 
