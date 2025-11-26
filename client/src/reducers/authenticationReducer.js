5c4d156f3055dc33401130a80d4c6d7bcb7bbbeb
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-26 14:11:15 -0700

    Create and export authenticationReducer function

---
+++ client/src/reducers/authenticationReducer.js
@@ -0,0 +1,7 @@
+// State initially starts off as undefined, so it's set here as an empty object
+export default function (state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
