e7f710273390e7ec464ced64c215d0ed965186fc
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-26 14:17:27 -0700

    Debug no anonymous default export warning

--- client/src/reducers/authenticationReducer.js
+++ client/src/reducers/authenticationReducer.js
@@ -1,7 +1,9 @@
-// State initially starts off as undefined, so it's set here as an empty object
-export default function (state = {}, action) {
+// State initially starts off as undefined, so it's set here as null instead
+const authenticationReducer = (state = null, action) => {
   switch (action.type) {
     default:
       return state;
   }
-}
+};
+
+export default authenticationReducer;
