41778a262ee0c3f93c1a9f3405a15f70c2b93002
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-26 13:30:46 -0700

    Set up React rendering logic of root div

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+
+const divElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(divElement);
+
+root.render(<App />);
