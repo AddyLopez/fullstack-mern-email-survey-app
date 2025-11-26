4f24405662b0758e69b5dd10732aa7b8d483087d
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-26 14:24:46 -0700

    Add annotation

--- client/src/reducers/authenticationReducer.js
+++ client/src/reducers/authenticationReducer.js
@@ -1,3 +1,5 @@
+// The purpose of monitoring this slice of state is to conditionally render UI components or content based on whether a user is logged in or not.
+
 // State initially starts off as undefined, so it's set here as null instead
 const authenticationReducer = (state = null, action) => {
   switch (action.type) {
