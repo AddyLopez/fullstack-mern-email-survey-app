0a3323e8016cdcbd6a0a7c4f3dade269cb64e837
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-28 14:59:50 -0700

    Add some content to Landing page component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <main>
+      <h1>Welcome to Inquire</h1>
+      <h2>An email survey application</h2>
+    </main>
+  );
+};
+
+export default Landing;
