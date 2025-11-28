2fd86c8edec930784a29764ba826917e98a7b293
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-28 16:14:19 -0700

    Import and pass in reduxThunk middleware

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
 // Argument 1: reducer(s). Argument 2: initial state for server-side rendering. Argument 3 for middleware.
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 const divElement = document.getElementById("root");
 const root = ReactDOM.createRoot(divElement);
