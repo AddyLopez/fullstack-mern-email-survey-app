08f0cc638096df6870be77d19fcbb9f9b3b0b916
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-28 15:07:47 -0700

    Add Outlet to Dashboard component for SurveyNew

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-  return <h4>I am the Dashboard component!</h4>;
+  return (
+    <section>
+      <h4>I am the Dashboard component!</h4>;
+      <Outlet />
+    </section>
+  );
 };
 
 export default Dashboard;
