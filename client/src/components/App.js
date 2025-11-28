7bbb02cec0014a6ac6a5f94ea32927b97b3ab152
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-28 14:41:50 -0700

    Debug rendering of routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Outlet,
   Route,
   RouterProvider,
 } from "react-router-dom";
 
 // dummy components
-const Root = () => <h2>Header</h2>;
-const Dashboard = () => <h2>Dashboard</h2>;
+const Root = () => (
+  <div>
+    <h2>Header</h2>
+    <Outlet />
+  </div>
+);
+const Dashboard = () => (
+  <div>
+    <h2>Dashboard</h2>
+    <Outlet />
+  </div>
+);
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
@@ -16,8 +27,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route path="/" element={<Landing />} />
-      <Route path="/surveys" element={<Dashboard />}>
-        <Route path="/surveys/new" element={SurveyNew} />
+      <Route path="surveys" element={<Dashboard />}>
+        <Route path="new" element={<SurveyNew />} />
       </Route>
     </Route>
   )
