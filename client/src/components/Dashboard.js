84db4dc6005dbd4024a8aa5d962cee4478f9fc6d
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-28 15:08:03 -0700

    Remove typo

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const Dashboard = () => {
   return (
     <section>
-      <h4>I am the Dashboard component!</h4>;
+      <h4>I am the Dashboard component!</h4>
       <Outlet />
     </section>
   );
