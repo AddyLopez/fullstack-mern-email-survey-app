e3d6056b23960dab1a5d6097a67e2f6dce15d944
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-28 14:54:52 -0700

    Complete Footer component with link to GitHub repo

---
+++ client/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      A full-stack web application.{" "}
+      <a
+        href="https://github.com/AddyLopez/fullstack-mern-email-survey-app"
+        target="_blank"
+        title="To GitHub repository"
+      >
+        Open-source code
+      </a>{" "}
+      by Addy López.
+    </footer>
+  );
+};
+
+export default Footer;
