d5e5b4c70f49730d8e947f5886853649541565eb
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-30 19:51:26 -0700

    Update Redux action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-// Redux action creator
+// Redux action creator. It dispatches the action after API responds.
 const fetchUser = () => {
-  // Make request to backend API
-  axios.get("/api/current_user");
+  return function (dispatch) {
+    // Make request to backend API
+    axios
+      .get("/api/current_user")
+      .then((response) => dispatch({ type: FETCH_USER, payload: response }));
+  };
 };
