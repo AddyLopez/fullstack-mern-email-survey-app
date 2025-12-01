0c5322ca7356406b3f6c33e14bd97f9c6eec00eb
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-30 21:01:56 -0700

    Refactor action creator with async/await syntax

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,10 +3,9 @@ import { FETCH_USER } from "./types";
 
 // Redux action creator. It uses redux thunk. It dispatches the action after API responds.
 export const fetchUser = () => {
-  return function (dispatch) {
-    // Make request to backend API
-    axios
-      .get("/api/current_user")
-      .then((response) => dispatch({ type: FETCH_USER, payload: response }));
+  return async (dispatch) => {
+    // Make request to backend API and dispatch action
+    const response = await axios.get("/api/current_user");
+    dispatch({ type: FETCH_USER, payload: response });
   };
 };
