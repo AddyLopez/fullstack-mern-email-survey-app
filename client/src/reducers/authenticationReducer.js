f1fb2264d5b9c7c7fad4d5d594e05c1c9f487d1d
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-30 20:44:46 -0700

    Add logging for testing

--- client/src/reducers/authenticationReducer.js
+++ client/src/reducers/authenticationReducer.js
@@ -2,6 +2,7 @@
 
 // State initially starts off as undefined, so it's set here as null instead
 const authenticationReducer = (state = null, action) => {
+  console.log(action);
   switch (action.type) {
     default:
       return state;
