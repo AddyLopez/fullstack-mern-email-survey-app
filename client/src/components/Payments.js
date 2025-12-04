503b63126b6a15d2f4c20133227c46452e91da1a
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-04 14:16:36 -0700

    Add button, description, and name

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,9 +6,13 @@ class Payments extends Component {
     return (
       <ReactStripeCheckout
         amount={500} // amount in cents
+        description="Pay $5 for 5 email credits"
+        name="Inquire"
         token={(token) => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button>Add Credits</button>
+      </ReactStripeCheckout>
     );
   }
 }
