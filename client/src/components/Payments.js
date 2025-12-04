55b8556b04fc864257899ec3b042828923a35fdb
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-03 22:42:20 -0700

    Remove debugger keyword

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -3,8 +3,6 @@ import ReactStripeCheckout from "react-stripe-checkout";
 
 class Payments extends Component {
   render() {
-    debugger;
-
     return (
       <ReactStripeCheckout
         amount={500} // amount in cents
