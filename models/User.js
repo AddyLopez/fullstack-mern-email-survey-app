1dc6e9b53c5c47125e58c919c5016f574ff1dafb
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-04 22:14:44 -0700

    Add credits property to Mongoose userSchema

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 // Define properties of user inside Users collection
 const userSchema = new Schema({
   googleId: String,
+  credits: { type: Number, default: 0 },
 });
 
 // (If "users" collection not already created then) Create users Model Class with the userSchema using Mongoose
