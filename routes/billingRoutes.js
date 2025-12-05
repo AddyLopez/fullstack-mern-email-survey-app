0139c59ddd4db0e280561b41094cf29a3b6e0c4a
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-04 22:05:59 -0700

    Refactor Promise with async await syntax

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,14 +2,16 @@ const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = (app) => {
-  app.post("/api/stripe", (req, res) => {
+  app.post("/api/stripe", async (req, res) => {
     // Handle token, reach out to Stripe API, finalize charge, update user's credits
     // console.log(req.body);
-    stripe.charges.create({
+    const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
       description: "$5 for 5 credits",
       source: req.body.id,
     });
+
+    console.log(charge);
   });
 };
