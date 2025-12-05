df1c6702127cef2e4484af93a68678f126550af7
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-04 21:12:01 -0700

    Add post route

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,1 +1,5 @@
-module.exports = (app) => {};
+module.exports = (app) => {
+  app.post("/api/stripe", (req, res) => {
+    // Handle token, reach out to Stripe API, finalize charge, update user's credits
+  });
+};
