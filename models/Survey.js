e3ec01ba12236398de8d18241892903310992a99
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-08 17:28:33 -0700

    Set up survey model class with mongoose

---
+++ models/Survey.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// Survey properties: title, body, subject, recipients
+const surveySchema = new Schema({
+  title: String,
+  body: String,
+  subject: String,
+  recipients: [String],
+});
+
+// If "surveys" collection not already created then create surveys Model Class with the surveySchema using Mongoose
+mongoose.model("surveys", surveySchema);
