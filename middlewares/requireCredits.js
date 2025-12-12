1474ea558bc350b8a5e2c50be2af241cef579a14
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-11 19:05:28 -0700

    Update HTTP status code since 402 is reserved

--- middlewares/requireCredits.js
+++ middlewares/requireCredits.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   if (req.user.credits < 1) {
-    return res.status(402).send({ error: "Insufficient credits." });
+    return res.status(403).send({ error: "Insufficient credits." });
   }
 
   next();
