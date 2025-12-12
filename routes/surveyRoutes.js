dd5d52dca0d3e9169f185e40261264ef4deb75ff
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-11 18:53:46 -0700

    Create POST route and inject middleware

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,1 +1,8 @@
-module.exports = (app) => {};
+const requireLogin = require("../middlewares/requireLogin");
+
+module.exports = (app) => {
+  app.post("/api/surveys", requireLogin, (req, res) => {
+    // Is user logged in?
+    // Do they have enough credits?
+  });
+};
