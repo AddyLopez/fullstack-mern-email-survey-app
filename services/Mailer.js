565185c370502c2b38420d60e63c777b54e60266
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-11 21:33:45 -0700

    Set up Mailer class with sendGrid library

---
+++ services/Mailer.js
@@ -0,0 +1,5 @@
+const sendGrid = require("sendgrid");
+const helper = sendGrid.mail; // Mail class from sendgrid library
+const keys = require("../config/keys");
+
+class Mailer extends helper.Mail {}
