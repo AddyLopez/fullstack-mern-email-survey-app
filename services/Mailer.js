1ed17244f3c24beb6ee05b8e04cb81627f8d92e4
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-12 17:06:51 -0700

    Set up Mailer class constructor

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,6 +2,15 @@ const sendGrid = require("sendgrid");
 const helper = sendGrid.mail; // Mail class from sendgrid library
 const keys = require("../config/keys");
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email(keys.sendGridFromEmail);
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+}
 
 module.exports = Mailer;
