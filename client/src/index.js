32fa7ba8c33c1349adf1b65e3ce7afa4d7e3bae9
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-13 12:26:19 -0700

    import axios to test out back-end route

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
+import axios from "axios";
+window.axios = axios; // temporary code to test out back-end route
 
 // Argument 1: reducer(s). Argument 2: initial state for server-side rendering. Argument 3 for middleware.
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
