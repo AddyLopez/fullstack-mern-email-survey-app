672c16fdd043d1efbcc74e26a235b84a36178785
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-19 14:49:40 -0700

    Import and interpolate redirectDomain key

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,5 @@
+const keys = require("../../config/keys");
+
 module.exports = (survey) => {
   // HTML to include in body of emailed survey
   return `
@@ -44,17 +46,12 @@ module.exports = (survey) => {
             <h2>Please answer the following question:</h2>
             <p>${survey.body}</p>
             <div>
-              <a href="http://localhost:3000" title="Yes">Yes</a>
-              <a href="http://localhost:3000" title="No">No</a>
+              <a href="${keys.redirectDomain}" title="Yes">Yes</a>
+              <a href="${keys.redirectDomain}" title="No">No</a>
             </div>
           </article>
         </div>
       </body>
     </html>
-
-
-
-
-
   `;
 };
