c12ed5ead522e1ce50821565f5ccca281df0d6f3
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-19 12:53:39 -0700

    Debugging successful! Fix of return statement

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/Mailer");
-const surveyTemplate = require("../services/email_templates/surveyTemplate");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("Survey");
 
@@ -15,8 +15,8 @@ module.exports = (app) => {
       title,
       subject,
       body,
-      recipients: recipients.split(",").map((email) => {
-        ({ email: email.trim() });
+      recipients: recipients.split(",").map((recipient) => {
+        return { email: recipient.trim() };
       }),
       _user: req.user.id,
       dateSent: Date.now(),
