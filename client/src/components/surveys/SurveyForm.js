d7d3a85bd3cd954402b0f015f1d9ac620d433468
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-20 13:25:27 -0700

    Import Field component and include attributes

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import { reduxForm, Field } from "redux-form";
 
 // SurveyForm shows a form for a user to add input
 class SurveyForm extends Component {
   render() {
-    return <p>I am the SurveyForm component!</p>;
+    return (
+      <>
+        <p>I am the SurveyForm component!</p>
+        <Field type="text" name="surveyTitle" component="input" />
+      </>
+    );
   }
 }
 
