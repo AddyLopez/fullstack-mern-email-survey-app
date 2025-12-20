4afd28f8dcfe7beccdc113a8200f43a81cabcbcc
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-20 13:06:20 -0700

    Import redux form reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
+import { reducer as reduxForm } from "redux-form";
 import authenticationReducer from "./authenticationReducer";
 
 export default combineReducers({
   // keys inside state object
   authentication: authenticationReducer,
+  form: reduxForm,
 });
