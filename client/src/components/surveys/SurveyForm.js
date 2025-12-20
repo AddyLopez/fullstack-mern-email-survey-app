a7712f504fc15a86e8c6212b16ba516710a5b890
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-20 13:33:41 -0700

    Add form and button tags and submit handling

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,11 +3,17 @@ import { reduxForm, Field } from "redux-form";
 
 // SurveyForm shows a form for a user to add input
 class SurveyForm extends Component {
+  // handleSubmit function is provided by reduxForm helper
   render() {
     return (
       <>
         <p>I am the SurveyForm component!</p>
-        <Field type="text" name="surveyTitle" component="input" />
+        <form
+          onSubmit={this.props.handleSubmit((values) => console.log(values))}
+        >
+          <Field type="text" name="surveyTitle" component="input" />
+          <button type="submit">Submit</button>
+        </form>
       </>
     );
   }
