a3de89d446e71dee64cf5548ff3fbc32a7a76fc1
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-20 13:19:37 -0700

    Convert to React class component for now

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 
 // SurveyForm shows a form for a user to add input
-const SurveyForm = () => {
-  return <p>I am the SurveyForm component!</p>;
-};
+class SurveyForm extends Component {
+  render() {
+    return <p>I am the SurveyForm component!</p>;
+  }
+}
 
-export default SurveyForm;
+export default reduxForm()(SurveyForm);
