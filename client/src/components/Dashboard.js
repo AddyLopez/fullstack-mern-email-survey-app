177e7f64ccd6564f4ed8af158e0a90808afd91f4
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-19 23:54:55 -0700

    Stylize Dashboard component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import "./styles/Dashboard.css";
 
 const Dashboard = () => {
   return (
-    <section>
+    <section className="Dashboard">
       <h4>I am the Dashboard component!</h4>
       <Outlet />
+      <div className="button-container">
+        <button>+</button>
+      </div>
     </section>
   );
 };
