94623c9c67b75cf2aef1cd899fda8e99e1ed794a
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-20 13:16:55 -0700

    Annotate purpose for clarity and render SurveyForm

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,8 +1,14 @@
 import React from "react";
 import SurveyForm from "./SurveyForm";
 
+// SurveyNew shows SurveyForm and SurveyFormReview
 const SurveyNew = () => {
-  return <p>I am the SurveyNew component!</p>;
+  return (
+    <>
+      <p>I am the SurveyNew component!</p>;
+      <SurveyForm />
+    </>
+  );
 };
 
 export default SurveyNew;
