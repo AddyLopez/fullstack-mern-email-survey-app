e2cd3e3d8c775cee36c8e2fba55f980bc3754bf0
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-21 23:28:50 -0700

    Display error message from meta prop value

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,11 +2,11 @@ import React from "react";
 
 // SurveyField contains logic to render a single text label and input
 const SurveyField = ({ input, label, meta }) => {
-  console.log(meta);
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+      {meta.error}
     </div>
   );
 };
