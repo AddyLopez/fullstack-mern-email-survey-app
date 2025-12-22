94c4d2518cf50429a08e58c625023f73d242bd18
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-22 14:48:48 -0700

    Supply regular expression to validate emails

--- client/src/utils/validateRecipients.js
+++ client/src/utils/validateRecipients.js
@@ -1,7 +1,21 @@
+const regex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 const validateRecipients = (recipients) => {
-  const recipientsArray = recipients.split(",").map((recipient) => {
-    return recipient.trim();
-  });
+  const invalidEmailAddresses = recipients
+    .split(",")
+    .map((recipient) => {
+      return recipient.trim();
+    })
+    .filter((recipient) => {
+      return regex.test(recipient) === false;
+    });
+
+  if (invalidEmailAddresses.length) {
+    return `Invalid email address(es): ${invalidEmailAddresses}`;
+  }
+
+  return;
 };
 
 export default validateRecipients;
