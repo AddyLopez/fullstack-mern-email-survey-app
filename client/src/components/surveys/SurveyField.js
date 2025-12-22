faab10211dc888c9089645e55fe5cc905de34f1e
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-21 21:06:44 -0700

    Wrap a div around label and input

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -3,10 +3,10 @@ import React from "react";
 // SurveyField contains logic to render a single text label and input
 const SurveyField = ({ input, label }) => {
   return (
-    <>
+    <div>
       <label>{label}</label>
-      <input {...input} />;
-    </>
+      <input {...input} />
+    </div>
   );
 };
 
