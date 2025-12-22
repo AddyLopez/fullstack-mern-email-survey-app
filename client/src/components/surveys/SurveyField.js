17af38fb612fff8cc56731d3299a100f82091aa5
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-21 20:55:55 -0700

    Add input props using spread syntax

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 // SurveyField contains logic to render a single text label and input
-const SurveyField = (props) => {
-  console.log(props.input);
-  return <input />;
+const SurveyField = ({ input }) => {
+  return (
+    <input {...input} />
+  ); /*Spread syntax adds all of input's properties as JSX props without having to be enumerated and named one by one*/
 };
 
 export default SurveyField;
