a7c713777b35429672aff680c02198cb6840a16d
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-21 20:56:08 -0700

    Remove annotation

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,9 +2,7 @@ import React from "react";
 
 // SurveyField contains logic to render a single text label and input
 const SurveyField = ({ input }) => {
-  return (
-    <input {...input} />
-  ); /*Spread syntax adds all of input's properties as JSX props without having to be enumerated and named one by one*/
+  return <input {...input} />;
 };
 
 export default SurveyField;
