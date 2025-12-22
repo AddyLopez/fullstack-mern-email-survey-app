d17045e948531ac46bd97dc431352779bd84bc49
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-21 20:19:22 -0700

    Create SurveyField component

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// SurveyField contains logic to render a single text label and input
+const SurveyField = () => {
+  return <p>I am the SurveyField component!</p>;
+};
+
+export default SurveyField;
