c9c03827b125410d7bb503fba42be3a4a1a6abc6
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-23 15:30:14 -0700

    Create button and onClick event

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const SurveyReview = () => {
+// SurveyFormReview shows users their form inputs for review
+const SurveyFormReview = ({ toggle }) => {
   return (
     <section>
       <h4>Please confirm that all the information is correct.</h4>
+      <button onClick={toggle}>Back</button>
     </section>
   );
 };
 
-export default SurveyReview;
+export default SurveyFormReview;
