7015c26114fd0c2f8d0167cda2c8a1f5ce2e535b
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-24 16:17:28 -0700

    Move formFields array into new file

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,20 @@
+const formFields = [
+  {
+    label: "Survey Title",
+    name: "surveyTitle",
+  },
+  {
+    label: "Subject Line",
+    name: "surveySubject",
+  },
+  {
+    label: "Email Body",
+    name: "surveyBody",
+  },
+  {
+    label: "Recipient List",
+    name: "surveyRecipients",
+  },
+];
+
+export default formFields;
