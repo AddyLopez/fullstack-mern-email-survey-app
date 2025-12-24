30df3ed4b0e023253205cc2cc1caa68dc96f9191
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-12-24 16:27:09 -0700

    Map over formFields to display values

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
+import formFields from "./formFields";
 
 // SurveyFormReview shows users their form inputs for review
 const SurveyFormReview = ({ toggle, formValues }) => {
+  const reviewFormFields = formFields.map((formField) => {
+    return (
+      <section key={formField.name}>
+        <div>
+          <label>{formField.label}</label>
+          <p>{formValues[formField.name]}</p>
+        </div>
+      </section>
+    );
+  });
+
   return (
     <section>
       <h4>Please confirm that all the information is correct.</h4>
+      {reviewFormFields}
       <button onClick={toggle}>Back</button>
     </section>
   );
