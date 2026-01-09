29a62af1644d04dad99843dd7e44f75d725181c4
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-01-08 23:25:27 -0700

    Update surveySchema for consistent naming

--- models/Survey.js
+++ models/Survey.js
@@ -4,10 +4,10 @@ const RecipientSchema = require("./Recipient");
 
 // Note the relationship between User and Survey models
 const surveySchema = new Schema({
-  title: String,
-  body: String,
-  subject: String,
-  recipients: [RecipientSchema],
+  surveyTitle: String,
+  surveyBody: String,
+  surveySubject: String,
+  surveyRecipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 },
   _user: { type: Schema.Types.ObjectId, ref: "User" },
