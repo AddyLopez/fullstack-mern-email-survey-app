89a28cea1cffb0f1a7d960e3221ef0164bd5df32
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-02-02 00:35:59 -0700

    Refactor default export of anonymous function

--- client/src/reducers/surveysReducer.js
+++ client/src/reducers/surveysReducer.js
@@ -2,11 +2,13 @@ import { FETCH_SURVEYS } from "../actions/types";
 // This reducer's purpose is to wait for the FETCH_SURVEYS action type and return the list of the current user's surveys.
 
 // Initialize surveys' slice of state to an empty array by default when application boots up.
-export default function (state = [], action) {
+const surveysReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_SURVEYS:
       return action.payload;
     default:
       return state;
   }
-}
+};
+
+export default surveysReducer;
