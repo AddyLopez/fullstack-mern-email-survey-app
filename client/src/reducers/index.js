3d5467f63846cd143ecd2e802840f1f963a98e81
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-02-02 00:25:41 -0700

    Wire up surveysReducer to combineReducers call

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { reducer as reduxForm } from "redux-form";
 import authenticationReducer from "./authenticationReducer";
+import surveysReducer from "./surveysReducer";
 
 export default combineReducers({
   // keys inside state object
   authentication: authenticationReducer,
   form: reduxForm,
+  surveys: surveysReducer,
 });
