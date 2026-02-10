373d10a90f2bb72055954519b9e04872a9c2ee74
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-02-10 01:19:19 -0700

    Eliminate plus button to improve UI/UX

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,18 +1,12 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import SurveyList from "./surveys/SurveyList";
-import "../styles/Dashboard.css";
 
 const Dashboard = () => {
   return (
     <section className="Dashboard">
       <SurveyList />
       <Outlet />
-      <div className="button-container">
-        <Link to="/surveys/new" className="button-plus">
-          +
-        </Link>
-      </div>
     </section>
   );
 };
