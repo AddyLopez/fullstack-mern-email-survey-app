8b6acf81a836fd1ca4346ee228fce9075e6c5652
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-02-11 21:11:31 -0700

    Reorganize JSX for responsive styling

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
 import validateRecipients from "../../utils/validateRecipients";
 import formFields from "./formFields";
+import "../../styles/SurveyForm.css";
 
 // SurveyForm shows a form for a user to add input
 class SurveyForm extends Component {
@@ -28,14 +29,22 @@ class SurveyForm extends Component {
   // handleSubmit function is provided by reduxForm helper
   render() {
     return (
-      <>
-        <p>I am the SurveyForm component!</p>
-        <form onSubmit={this.props.handleSubmit(this.props.toggle)}>
-          {this.renderFields()}
-          <Link to="/surveys">Cancel</Link>
-          <button type="submit">Next</button>
-        </form>
-      </>
+      <div className="SurveyForm">
+        <h3>NEW SURVEY</h3>
+        <h4>
+          Fill in the form to create a survey. Select "Next" to review your
+          answers.
+        </h4>
+        <div className="form-div">
+          <form onSubmit={this.props.handleSubmit(this.props.toggle)}>
+            {this.renderFields()}
+            <div className="form-buttons">
+              <Link to="/surveys">CANCEL</Link>
+              <button type="submit">NEXT</button>
+            </div>
+          </form>
+        </div>
+      </div>
     );
   }
 }
