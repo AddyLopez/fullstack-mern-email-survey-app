3aa1510a1c9d1b599d7ae56278be43cb33d10ef0
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-02-17 23:58:54 -0700

    comment out Sendgrid code, update with mailgun-js

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,10 +1,24 @@
-const sendGrid = require("sendgrid");
-const helper = sendGrid.mail; // Mail class from SendGrid library
+const Mailgun = require("mailgun-js");
+//const mailgun = new Mailgun(FormData);
+// const sendGrid = require("sendgrid");
+// const helper = sendGrid.mail; // Mail class from SendGrid library
 const keys = require("../config/keys");
 
-class Mailer extends helper.Mail {
+class Mailer /*extends helper.Mail*/ {
   constructor({ subject, recipients }, content) {
-    super();
+    this.mailgun = mailgun({
+      apiKey: keys.mailgunKey,
+      domain: keys.mailgunDomain,
+    });
+
+    this.data = {
+      from: "shandelaide@gmail.com",
+      to: this.formatAddresses(recipients),
+      subject: subject,
+      html: content,
+      "o:tracking-clicks": true,
+    };
+    /*super();
 
     this.sgAPI = sendGrid(keys.sendGridKey); // Pass in API key to communicate with sendGrid API
     this.from_email = new helper.Email(keys.sendGridFromEmail);
@@ -15,14 +29,18 @@ class Mailer extends helper.Mail {
     this.addContent(this.body); // Built-in functionality from SendGrid's helper.Mail base class
     this.addClickTracking(); // To have SendGrid keep track of which recipient clicked a link in the email
     this.addRecipients(); // Add recipients to the mailer
+    */
   }
 
   formatAddresses(recipients) {
-    return recipients.map(({ email }) => {
-      return new helper.Email(email); // Format each recipient's email with SendGrid's helper function
-    });
+    return recipients
+      .map(({ email }) => {
+        return email; /* new helper.Email(email); // Format each recipient's email with SendGrid's helper function */
+      })
+      .join(",");
   }
 
+  /*
   addClickTracking() {
     // SendGrid boilerplate (not well-documented)
     const trackingSettings = new helper.TrackingSettings();
@@ -41,8 +59,19 @@ class Mailer extends helper.Mail {
     // Add completed personalization object
     this.addPersonalization(personalize); // Built-in functionality from SendGrid's helper.Mail base class
   }
+  */
 
   async send() {
+    return new Promise((resolve, reject) => {
+      this.mailgun.messages().send(this.data, (error, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(body);
+        }
+      });
+    });
+    /*
     try {
       // toJSON and API methods are defined by base class
       const request = this.sgAPI.emptyRequest({
@@ -57,6 +86,7 @@ class Mailer extends helper.Mail {
     } catch (error) {
       console.log("error", error.message);
     }
+    */
   }
 }
 
